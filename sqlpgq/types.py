777623775e2f1d4a42237414d4fcd60a7708243a
James Cagalawan <james.cagalawan@gmail.com>
2025-12-06 21:07:38 -0500

    Generate basic queries

---
+++ sqlpgq/types.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+
+class SQLType:
+    sql_type: str = "TEXT"
+
+
+class Integer(SQLType):
+    sql_type = "INTEGER"
+
+
+class String(SQLType):
+    sql_type = "TEXT"
+
+
+class Float(SQLType):
+    sql_type = "REAL"
+
+
+class Boolean(SQLType):
+    sql_type = "BOOLEAN"
+
+
+class Date(SQLType):
+    sql_type = "DATE"
+
+
+class DateTime(SQLType):
+    sql_type = "TIMESTAMP"
